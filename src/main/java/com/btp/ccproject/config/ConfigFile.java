bf35ee9eff735022e8230b6fa2291d9c4af43158
MadhavKabra10 <madhavkabra57@gmail.com>
2025-04-10 17:09:03 +0530

    Making project command line interactive

--- src/main/java/com/btp/ccproject/config/ConfigFile.java
+++ src/main/java/com/btp/ccproject/config/ConfigFile.java
@@ -1,23 +1,23 @@
-package com.btp.ccproject.config;
-
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.core.DockerClientBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ConfigFile {
-    @Bean
-    DockerClient dockerClientDefault(){
-        return DockerClientBuilder.getInstance().build();
-    }
-
+//package com.btp.ccproject.config;
+//
+//import com.github.dockerjava.api.DockerClient;
+//import com.github.dockerjava.core.DockerClientBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ConfigFile {
 //    @Bean
-//    DockerClient dockerClientCustom(){
-//        return DockerClientBuilder.getInstance(
-//                        DefaultDockerClientConfig.createDefaultConfigBuilder()
-//                                .withDockerHost("tcp://localhost:2375")
-//                                .build()
-//                ).build();
+//    DockerClient dockerClientDefault(){
+//        return DockerClientBuilder.getInstance().build();
 //    }
-}
+//
+////    @Bean
+////    DockerClient dockerClientCustom(){
+////        return DockerClientBuilder.getInstance(
+////                        DefaultDockerClientConfig.createDefaultConfigBuilder()
+////                                .withDockerHost("tcp://localhost:2375")
+////                                .build()
+////                ).build();
+////    }
+//}
